1173b091a423abb433c6ac68a12f5b32d7ef09b8
japipe05 <89116331+japipe05@users.noreply.github.com>
2023-01-21 05:16:44 -0500

    fin portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,25 @@
 /*=============== SHOW SIDEBAR ===============*/
+const navMenu = document.getElementById('sidebar'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 
+ 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
+if(navToggle){
+    navToggle.addEventListener("click", ()=> {
+        navMenu.classList.add('show-sidebar')
+    })
+}
 
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-
+if(navClose){
+    navClose.addEventListener("click", ()=> {
+        navMenu.classList.remove('show-sidebar')
+    })
+}
 
 /*=============== SKILLS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
@@ -120,7 +132,7 @@ function focusFunc(){
     let parent = this.parentNode;
     parent.classList.add("focus");
 }
-
+ 
 function blurFunc(){
     let parent = this.parentNode;
     if(this.value==""){
